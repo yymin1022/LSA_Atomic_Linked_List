480134536f1ce90a0aaf71a478ad54e6a6ca2547
yymin1022 <yymin1022@gmail.com>
2023-12-03 04:43:59 +0000

    Update Module : Added Basic Functions

--- LinkedListWithSync/ll_sync_module_main.c
+++ LinkedListWithSync/ll_sync_module_main.c
@@ -1,6 +1,8 @@
+#include <linux/delay.h>
+#include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/init.h>
+#include <linux/kthread.h>
 
 #include "linked_list_impl.h"
 
@@ -13,13 +15,32 @@ int	range_bound[4][2] = {
 	{750000, 999999}
 };
 
-int	__init atomic_ll_init(void)
+static int	list_iter(int *tid, int range_idx)
 {
+	int	*iter_range;
+	void	*list_head;
+
+	iter_range = range_bound[range_idx];
+	list_head = add_to_list(*tid, iter_range);
+	search_list(*tid, list_head, iter_range);
+	del_from_list(*tid, iter_range);
+	while (!kthread_should_stop())
+		msleep(500);
+	printk (KERN_INFO "Thread #%d Stopped!\n", *tid);
+	return (0);
+}
+
+int		__init atomic_ll_init(void)
+{
+	int	tid;
+
+	tid = 1234;
+	list_iter(&tid, 0);
 	printk ("Linked List with Sync 20194094 Yongmin Yoo\n");
 	return (0);
 }
 
-void	__exit atomic_ll_cleanup(void)
+void		__exit atomic_ll_cleanup(void)
 {
 	printk ("Linked List with Sync Module Removed\n");
 }

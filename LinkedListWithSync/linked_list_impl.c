340f6f38a340d85e181c5b2840989316d21c7593
yymin1022 <yymin1022@gmail.com>
2023-12-03 11:16:06 +0000

    Update linked_list_impl : Added List Functions Logic

--- LinkedListWithSync/linked_list_impl.c
+++ LinkedListWithSync/linked_list_impl.c
@@ -1,30 +1,50 @@
 #include "linked_list_impl.h"
 
 DEFINE_SPINLOCK(ll_lock);
+LIST_HEAD(global_list);
 
 void	*add_to_list(int thread_id, int *range_bound)
 {
+	int		i;
+	list_item	*tmp_elem;
+
 	spin_lock(&ll_lock);
+	for(i = range_bound[0]; i <= range_bound[1]; i++)
+	{
+		tmp_elem = kmalloc(sizeof(list_item), GFP_KERNEL);
+		tmp_elem->value = i;
+		list_add_tail(&tmp_elem->list, &global_list);
+	}
 	printk("Thread #%d Insert Range: %d ~ %d\n", thread_id, range_bound[0], range_bound[1]);
-	msleep(100);
 	spin_unlock(&ll_lock);
 	return NULL;
 }
 
 void	*search_list(int thread_id, void *data, int *range_bound)
 {
+	struct list_head	*cur;
+	struct list_head	*tmp;
+
 	spin_lock(&ll_lock);
+	list_for_each_safe(cur, tmp, &global_list);
 	printk("Thread #%d Search Range: %d ~ %d\n", thread_id, range_bound[0], range_bound[1]);
-	msleep(100);
 	spin_unlock(&ll_lock);
 	return NULL;
 }
 
 void	*del_from_list(int thread_id, int *range_bound)
 {
+	struct list_head	*cur;
+	struct list_head	*tmp;
+	list_item		*elem;
+
 	spin_lock(&ll_lock);
+	list_for_each_safe(cur, tmp, &global_list)
+	{
+		elem = list_entry(cur, list_item, list);
+		list_del(cur);
+	}
 	printk("Thread #%d Delete Range: %d ~ %d\n", thread_id, range_bound[0], range_bound[1]);
-	msleep(100);
 	spin_unlock(&ll_lock);
 	return NULL;
 }
--- LinkedListWithSync/linked_list_impl.h
+++ LinkedListWithSync/linked_list_impl.h
@@ -3,8 +3,14 @@
 # include <linux/delay.h>
 # include <linux/list.h>
 # include <linux/module.h>
+# include <linux/slab.h>
 # include <linux/spinlock.h>
 # include "calclock.h"
+
+typedef struct {
+	struct list_head list;
+	int value;
+}	list_item;
 
 void	*add_to_list(int thread_id, int *range_bound);
 void	*search_list(int thread_id, void *data, int *range_bound);
